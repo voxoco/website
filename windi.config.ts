eaec28784a01c0ad259338bd48acf74fdb2d08a4
Joe Mordica <jmordica@gmail.com>
2021-09-09 22:59:46 -0500

    initial commit and playing with tailwind ui

--- windi.config.ts
+++ windi.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   darkMode: 'class',
   // https://windicss.org/posts/v30.html#attributify-mode
   attributify: true,
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 })
