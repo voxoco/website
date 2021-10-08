a6c5d93ad5fd22475227f2a74716bac364dae24b
James Harrod <docharrod85@gmail.com>
2021-10-07 21:26:56 -0500

    test cms with team

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <main class="bg-white overflow-x-hidden">
+  <main v-if="$route.name !== 'admin'" class="bg-white overflow-x-hidden">
     <Header />
     <router-view />
     <Footer />
+  </main>
+  <main v-else>
+    <router-view />
   </main>
 </template>
---
+++ src/pages/admin.vue
@@ -0,0 +1,64 @@
+<script>
+
+import CMS from 'netlify-cms-app'
+
+const config = {
+  backend: {
+    name: 'github',
+    repo: 'voxoco/website',
+  },
+  media_folder: 'public/cms',
+  collections: [
+    {
+      name: 'Main',
+      label: 'VOXO Website',
+      files: [
+        {
+          label: 'Team',
+          name: 'Team',
+          file: 'public/cms/team.json',
+          fields: [
+            {
+              label: 'Team',
+              name: 'teamList',
+              widget: 'list',
+              fields: [
+                {
+                  label: 'Name',
+                  name: 'name',
+                  widget: 'string',
+                },
+                {
+                  label: 'Image',
+                  name: 'imageUrl',
+                  widget: 'image',
+                },
+                {
+                  label: 'Role',
+                  name: 'role',
+                  widget: 'string',
+                },
+                {
+                  label: 'Bio',
+                  name: 'bio',
+                  widget: 'text',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+export default {
+  setup() {
+    return {}
+  },
+  mounted() {
+    CMS.init({ config })
+  },
+}
+
+</script>
